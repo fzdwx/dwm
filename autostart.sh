176753fb5e245103f4c2a5200c30d6312bec041a
fzdwx <likelovec@gmail.com>
2023-02-05 12:28:35 +0800

    Update autostart

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# DWM自启动脚本 仅作参考 
+# DWM自启动脚本 仅作参考
 # 搭配 https://github.com/yaocccc/scripts 仓库使用 目录位置 ~/scripts
 # 部分配置文件在 ~/scripts/config 目录下
 
@@ -21,17 +21,10 @@ daemons() {
     dunst -conf $DWM/conf/dunst.conf & # 开启通知server
     picom --experimental-backends --config $DWM/conf/picom.conf >> /dev/null 2>&1 & # 开启picom
     /opt/clash/cfw &
+    feh --randomize --bg-fill ~/Pictures/bg/*
+    resetDns
 }
 
-cron() {
-    [ $1 ] && sleep $1
-    let i=10
-    while true; do
-        [ $((i % 300)) -eq 0 ] && feh --randomize --bg-fill ~/Pictures/bg/* # 每300秒更新壁纸
-        sleep 10; let i+=10
-    done
-}
 
 settings 1 &                                  # 初始化设置项
 daemons 3 &                                   # 后台程序项
-cron 5 &                                      # 定时任务项
