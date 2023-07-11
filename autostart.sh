dfde4c5211a36f76ab13d88e3b74b19b797575ff
fzdwx <likelovec@gmail.com>
2023-07-11 20:55:07 +0800

    sync

--- autostart.sh
+++ autostart.sh
@@ -17,11 +17,11 @@ daemons() {
 	[ $1 ] && sleep $1
 	$DWM/statusbar/statusbar.sh cron &                                             # 开启状态栏定时更新
 	fcitx5 &                                                                       # 开启输入法
-	flameshot &         
+	flameshot &
 	ask server 1333 &                                                           # 截图要跑一个程序在后台 不然无法将截图保存到剪贴板
 	dunst -conf $DWM/conf/dunst.conf &                                             # 开启通知server
 	picom --config $DWM/conf/picom.conf.bak >>/dev/null 2>&1 & # 开启picom
-	#/usr/bin/cfw &
+	launcher &
 	feh --randomize --bg-fill ~/Pictures/bg/*
 }
 
